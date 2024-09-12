0f844fd5938639df4d3d35936bd52f7cf89f2d85
khaled frayji <121765804+khaledfrayji@users.noreply.github.com>
2024-09-12 19:36:19 +0300

    version 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,32 @@
+"use client";
 
-export default function Home() {
+import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import { navItems } from "@/data";
+import Grid from "@/components/Grid";
+import Footer from "@/components/Footer";
+import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
+import RecentProjects from "@/components/RecentProjects";
+import Approach from "@/components/Approach";
+import Chatbot from "@/components/Chatbot";
+
+const Home = () => {
   return (
-    <main>
-
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        <RecentProjects />
+        <Experience />
+        <Approach />
+        <Clients />
+        <Chatbot />
+        <Footer />
+      </div>
     </main>
   );
-}
+};
+
+export default Home;
---
+++ data/index.ts
@@ -0,0 +1,225 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I value partnership with clients, ensuring clear and ongoing communication",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently pursuing my Master's degree",
+      description: "Academic Journey",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Dental website",
+      des: "A professional website for dental practices, featuring appointment scheduling, patient information management, and a gallery of dental services.",
+      img: "/smiley.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+      link: "https://kf-smiley.netlify.app",
+    },
+    {
+      id: 2,
+      title: "Campaign website",
+      des: "A dynamic campaign website designed for effective promotion and engagement, featuring interactive elements and seamless multimedia integration.",
+      img: "/camp.png",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+      link: "https://kf-camp.netlify.app",
+    },
+    {
+      id: 3,
+      title: "Education platform",
+      des: "An advanced educational platform with AI-driven features, payment systems, and credit management, designed to enhance the learning experience.",
+      img: "/educonnect.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/c.svg"],
+      link: "https://kf-educonnect.netlify.app/",
+    },
+    {
+      id: 4,
+      title: "Weather app",
+      des: "A weather application built with React for an engaging and interactive user experience.",
+      img: "/weather.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg"],
+      link: "https://kf-weather.netlify.app",
+    },
+  ];
+  
+  
+  export const testimonials = [
+    {
+      quote:
+        "Working with Khaled was an exceptional experience. His meticulous attention to detail, commitment to quality, and quick response time were impressive. Khaled's passion for every aspect of web development shines through. If you're looking to elevate your online presence, Khaled is the right choice.",
+      name: "أحمد الخطيب",
+      title: "مدير تقنية المعلومات في تكنولوجيا ألفا",
+    },
+    {
+      quote:
+        "Khaled's expertise and creativity brought our project to life. His ability to understand our vision and transform it into a functional, beautiful website was remarkable. Khaled's dedication to his craft sets him apart. Highly recommended for anyone seeking top-notch web solutions.",
+      name: "سارة المصري",
+      title: "مديرة العمليات في حلول التقنية المتقدمة",
+    },
+    {
+      quote:
+        "Khaled's professionalism and technical skills were evident throughout our collaboration. He consistently delivered high-quality results on time. His friendly approach and clear communication made the process smooth and enjoyable. Khaled is a standout web designer and developer.",
+      name: "يوسف الحاج",
+      title: "رئيس قسم تطوير الأعمال في تيك جلوبال",
+    },
+    {
+      quote:
+        "Choosing Khaled for our website redesign was the best decision. His innovative ideas, combined with his thorough understanding of web technologies, helped us achieve a modern, user-friendly site. Khaled is a talented professional who truly cares about his clients' success.",
+      name: "منى صقر",
+      title: "مديرة التسويق في الحلول الرقمية",
+    },
+    {
+      quote:
+        "Khaled's work exceeded our expectations. His dedication to delivering a polished, functional website on time was evident from start to finish. Khaled's approach is client-focused, ensuring that every detail aligns with the client's vision. I highly recommend him for any web development project.",
+      name: "خالد الحمادي",
+      title: "مدير المشاريع في البرمجيات الذكية",
+    },
+  ];
+  
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Frontend Engineer Intern",
+      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Full-Stack Developer - Freelance",
+      desc: "Developed a full-stack web application for a client, implementing responsive designs and optimizing performance using React, Node.js, and MongoDB.",
+      className: "md:col-span-2",
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Private Instructor & Web Designer",
+      desc: "Provided private instruction to university and high school students in programming and web design while creating custom websites focusing on high proficiency and security.",
+      className: "md:col-span-2",
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Lead Frontend Developer",
+      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      link: "https://github.com/khaledfrayji",
+    },
+    {
+      id: 2,
+      img: "/insta.svg",
+       link: "https://instagram.com/khaled.frayji",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+       link: "https://www.linkedin.com/in/khaledfrayji/",
+    },
+  ];
